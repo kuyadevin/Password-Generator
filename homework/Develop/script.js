3b955a8a77a4356874f39c0e6e3bc2df554605ab
KuyaDevin <devinnguyen@ucsb.edu>
2022-03-31 18:37:50 -0700

    Added length prompt and its responses

---
+++ homework/Develop/script.js
@@ -0,0 +1,40 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  // function generate password was undefined
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  // GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+var passwordStart=prompt("How many characters would you like?");
+
+console.log(passwordStart)
+
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+if (passwordStart > 8 && passwordStart < 128){
+  confirm("Would you like lowercase letters?")
+} else alert("Your password must be between 8 and 128 characters long.")
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+}
