c75e80603a8dfaa78f9a8c558cb4e2f5a434aa8d
KuyaDevin <devinnguyen@ucsb.edu>
2022-09-26 20:50:46 -0700

    Add array with lowercase and uppercase letters

--- homework/Develop/script.js
+++ homework/Develop/script.js
@@ -18,15 +18,14 @@ function generatePassword() {
   }
 
   var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
-
   var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
-
   var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
-
   var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
 
   var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var symbolList = ["!", "@", "#", "$", "%", "^", "&", "8"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 }
 
 // Write password to the #password input
