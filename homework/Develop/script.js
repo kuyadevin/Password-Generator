e87494b3cc4489eb74cee3d32b6aab2e2b691481
KuyaDevin <devinnguyen@ucsb.edu>
2022-09-26 17:39:45 -0700

    Set up initial window prompt with number recognition

--- homework/Develop/script.js
+++ homework/Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  var userInput = window.prompt("How many characters would you like your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("That is not a number!")
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
